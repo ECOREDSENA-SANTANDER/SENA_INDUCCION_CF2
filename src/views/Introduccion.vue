b734ab1c0999e27a8330f7e6523fac251e41f593
Andrea Botello <andrea.botellor@misena.edu.co>
2024-04-12 11:57:00 -0500

    Entrega

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,7 +8,17 @@
         span
           i.fas.fa-info
       h1 Introducción
+
+    .row.align-items-center.justify-content-center
+      .col-lg-9(data-aos="fade-right")
+        .cajon.color-primario.p-4.py-md-5.pe-lg-5.mb-4.mb-lg-0
+          p.mb-0.pe-lg-5 Conocer la caracterización de la formación virtual en el SENA le permitirá a usted como aprendiz hacer uso de los recursos dispuestos para su formación, comprender el compromiso que ha adquirido para su proceso formativo y explorar las diferentes oportunidades de formación que le brinda el SENA, además de las oportunidades de inserción al mercado laboral colombiano.
+      .col-lg-3.col-md-7.col-10(data-aos="fade-left")
+        .img-esp
+          img(src='@/assets/curso/temas/intro.png', alt='')
     
+
+
 </template>
 
 <script>
--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,6 +7,15 @@
         span 1
       h1 La formación virtual en el SENA
 
+    .row
+      .col-lg-5.pe-lg-0(data-aos="fade-right")
+        img(src='@/assets/curso/temas/img.png', alt='')
+      .col-lg-7.ps-lg-0(data-aos="fade-left")
+        .bg-1.p-4.h-100.align-items-center.d-flex
+          .bg-white.p-4
+            p El SENA desde su oferta académica busca facilitar el acceso a la formación en diversos ambientes y modalidades. En relación con los ambientes de formación, el Aprendiz según sus necesidades, podrá elegir programas presenciales, virtuales o com
+            p.mb-0 En los programas de formación titulada, encontramos planes técnicos, tecnológicos y especializaciones tecnológicas.
+
 
     Separador
     #t_1_1
@@ -16,6 +25,7 @@
     p(data-aos="fade") La formación profesional Integral se centra en generar competencias que le permitan al egresado integrar tecnologías, plantear y solucionar creativamente problemas para lograr eficacia en el saber hacer, además brindarle movilidad en la estructura ocupacional.
     p(data-aos="fade") En este orden de ideas, las competencias se desarrollan de manera permanente en el proceso formativo, su punto de partida lo constituye el proyecto de vida personal del Aprendiz, estas se contextualizan en el marco de un Programa de Formación y se evidencian en los entornos sociales y productivos en el cual el Aprendiz interviene mediante sus desempeños.
 
+
     Separador
     #t_1_2
     .titulo-segundo.color-acento-contenido(data-aos="fade-right")
@@ -24,7 +34,26 @@
     p(data-aos="fade") La educación virtual hace referencia a los procesos de enseñanza-aprendizaje-evaluación que se llevan a cabo a partir de la interacción de sus actores en un ambiente mediado por las tecnologías de la información y comunicación -TIC.
     p.mb-5(data-aos="fade") En este sentido, el SENA desde su oferta de formación virtual posibilita a los aprendices la interacción en un ambiente de aprendizaje en donde contarán con el acompañamiento y orientación de sus instructores, así como también, la opción de ajustar sus horarios de estudio según convenga a sus necesidades y acorde con el cronograma de trabajo propuesto.
     
-    H4.mb-4 A continuación, se destacan las principales características de esta modalidad:
+    H4.mb-4(data-aos="fade-right") A continuación, se destacan las principales características de esta modalidad:
+
+    .row.align-items-center
+      .col-lg-6(data-aos="fade-right")
+        img(src='@/assets/curso/temas/tema1-2_img.svg', alt='')
+      .col-lg-6(data-aos="fade-left")
+        ol.lista-ol.lista-ol--separador.fa-ul.tema1-2
+          li.py-3 
+            span.text-bold 1 
+            | Acceso a la información en diversos formatos y en los tiempos que requiera el aprendiz.
+          li.py-3 
+            span.text-bold 2 
+            | Comunicación entre sus compañeros e instructores a través de diversas herramientas como: foros, chat, correo electrónico, mensajería interna y sesiones en línea.
+          li.py-3 
+            span.text-bold 3 
+            | Participación en diversas actividades dinamizadas a través de didácticas activas, fomentando en el aprendiz el interés por el aprendizaje autónomo y colaborativo.
+          li.py-3 
+            span.text-bold 4 
+            | Posibilidad de mejorar continuamente su proceso formativo, dando significado a sus aprendizajes a partir de la retroalimentación oportuna frente al desarrollo de sus actividades.
+
 
     Separador
     #t_1_3
@@ -33,30 +62,28 @@
     
     p.mb-5(data-aos="fade") De acuerdo con el Ministerio de Educación Nacional, la formación virtual tiene múltiples fortalezas entre ellas, que se ajusta a las necesidades y tiempos de los aprendices, permitiéndoles realizar un proceso educativo en cualquier momento y lugar. En este sentido, facilita el acceso a la educación a las personas que se encuentran en zonas de difícil acceso, trabajan y requieren una mayor flexibilidad de horarios.
     
-    .tarjeta--container.row.mb-5
+    .tarjeta--container.tema1-3.row.mb-5(data-aos="fade-down")
       .col-md.tarjeta.color-primario.p-5
         .row.justify-content-center.mb-4
           .col-6
             figure
               img(src='@/assets/curso/temas/icono1.svg', alt='')
-          
-        p.text-1.text-center Permite llegar a las regiones más apartadas, traspasar fronteras, apoyar a las personas que tienen menos recursos económicos con educación de alta calidad. 
+        p.text-1.text-center.mb-0 Permite llegar a las regiones más apartadas, traspasar fronteras, apoyar a las personas que tienen menos recursos económicos con educación de alta calidad. 
 
       .col-md.tarjeta.color-acento-contenido.p-5
         .row.justify-content-center.mb-4
           .col-6
             figure
               img(src='@/assets/curso/temas/icono2.svg', alt='')
-        
-        p.text-1.text-center Favorece el desarrollo de competencias en el uso de la tecnología, el aprendizaje autónomo, el trabajo en equipo y la posibilidad de conocer experiencias e intercambiar con compañeros de diferentes regiones, generando habilidades adicionales para ingresar al mercado laboral con mayor facilidad.
+        p.text-1.text-center.mb-0 Favorece el desarrollo de competencias en el uso de la tecnología, el aprendizaje autónomo, el trabajo en equipo y la posibilidad de conocer experiencias e intercambiar con compañeros de diferentes regiones, generando habilidades adicionales para ingresar al mercado laboral con mayor facilidad.
 
       .col-md.tarjeta.color-primario.p-5
         .row.justify-content-center.mb-4
           .col-6
             figure
               img(src='@/assets/curso/temas/icono3.svg', alt='')
-          
-        p.text-1.text-center Facilita el acceso a nuevos objetos de conocimiento: materiales interactivos y dinámicos que propician la comprensión de diversos temas.   
+        p.text-1.text-center.mb-0 Facilita el acceso a nuevos objetos de conocimiento: materiales interactivos y dinámicos que propician la comprensión de diversos temas.   
+
 
     Separador
     #t_1_4
@@ -65,11 +92,66 @@
 
     p(data-aos="fade") Para participar en la formación titulada y a distancia con experticia y pertinencia, es necesario reconocer conceptos institucionales propios de la modalidad. En este caso, socializamos los siguientes: 
     
+    .row.justify-content-center
+      .col-lg-10
+        AcordionA.tema1-4(tipo="b" clase-tarjeta="tarjeta bg-white")
+          .row.align-items-center(titulo="Programa formativo")
+            .col-md-7.mb-4.mb-md-0
+              p.mb-0 Se trata de la tecnología o especialización tecnológica que el instructor liderará. Su construcción obedece a una necesidad del sector empresarial.
+            .col-md-2
+            .col-md-2
+              img.w-50(src='@/assets/curso/temas/tema1-4_ico1.svg', alt='')
+          .row.align-items-center(titulo="Diseño curricular")
+            .col-md-8.mb-4.mb-md-0
+              p.mb-0 Es un documento que muestra la estructura general del programa, precisa características y proyecciones del contexto laboral y ocupacional, competencias que lo conforman, resultados de aprendizaje y tipo de formación. 
+            .col-md-1
+            .col-md-2
+              img.w-50(src='@/assets/curso/temas/tema1-4_ico2.svg', alt='')
+          .row.align-items-center(titulo="Planeación pedagógica")
+            .col-md-8.mb-4.mb-md-0
+              p.mb-0 Antes de entrar a realizar un proceso de formación es indispensable realizar la caracterización, de tal forma que se identifiquen aspectos como: necesidades de formación de los aprendices, estilos de aprendizaje y adaptación de los currículos, los lineamientos institucionales y la legislación vigente. 
+            .col-md-1
+            .col-md-2
+              img.w-75(src='@/assets/curso/temas/tema1-4_ico3.svg', alt='')
+          .row.align-items-center(titulo="Proyecto formativo")
+            .col-md-8.mb-4.mb-md-0
+              p.mb-0 Acciones integradoras e integradas entre sí, realizadas por los aprendices con la orientación del instructor a lo largo del proceso formativo. Son objeto directo de aprendizaje; esto indica que existe una relación directa entre lo que se debe hacer en el centro formativo y lo que más tarde se hará en el mundo laboral. 
+            .col-md-1
+            .col-md-2
+              img.w-75(src='@/assets/curso/temas/tema1-4_ico4.svg', alt='')
+          .row.align-items-center(titulo="Resultado de aprendizaje")
+            .col-md-8.mb-4.mb-md-0
+              p.mb-0 El resultado de aprendizaje presenta una relación de las evidencias junto con los resultados y su respectiva ponderación dentro del proceso de evaluación. 
+            .col-md-1
+            .col-md-2
+              img.w-50(src='@/assets/curso/temas/tema1-4_ico5.svg', alt='')
+          .row.align-items-center(titulo="Actividad de aprendizaje")
+            .col-md-8.mb-4.mb-md-0
+              p.mb-0 Acciones integradoras e integradas entre sí, realizadas por los aprendices con la orientación del instructor a lo largo del proceso formativo. Son objeto directo de aprendizaje; esto indica que existe una relación directa entre lo que se debe hacer en el Centro formativo y lo que más tarde se hará en el mundo laboral. 
+            .col-md-1
+            .col-md-2
+              img.w-75(src='@/assets/curso/temas/tema1-4_ico6.svg', alt='')
+
     
     Separador
     #t_1_5
     .titulo-segundo.color-acento-contenido(data-aos="fade-right")
       h2 1.5  Evaluación
+
+    .row.align-items-center.justify-content-center
+      .col-lg-8(data-aos="fade-right")
+        .pe-lg-5
+          p.pe-lg-5 La evaluación del aprendizaje en la institución se realiza a través del enfoque por competencias.
+          p.pe-lg-5 Es el proceso por el cual los instructores-tutores obtienen y analizan las evidencias del proceso de aprendizaje y desarrollo de las competencias del aprendiz, con base en el programa de formación y las normas de competencia laboral que le cobijan, con la finalidad de emitir el juicio si ha logrado el desarrollo de la competencia.
+          p.pe-lg-5.mb-4 La finalidad de la evaluación está dirigida al mejoramiento del desarrollo de las competencias del aprendiz.
+        .cajon.color-primario.p-4.py-md-5.ps-lg-5.pe-lg-5.mb-4.mb-lg-0
+          p.mb-0.pe-lg-5 Se evalúa con el   modelo de evaluación definido por el SENA.
+          p.mb-0.pe-lg-5.ps-3 #[b A =] APROBADO.
+          p.mb-0.pe-lg-5.ps-3 #[b D =] DEFICIENTE.
+      .col-lg-4.col-md-7.col-10(data-aos="fade-left")
+        .img-esp
+          img(src='@/assets/curso/temas/tema1-5_img.png', alt='')
+
 
     Separador
     #t_1_6
@@ -77,7 +159,77 @@
       h2 1.6  Oportunidades de formación e inserción laboral
     
     p(data-aos="fade") El egresado SENA es por definición una persona con una formación integral, es decir, que además de ser un gran profesional, formado con los más altos estándares del mercado mundial, también es una persona formada en valores y en habilidades blandas.
-    p(data-aos="fade") El SENA tiene el espacio para que el egresado siga vinculado con la Entidad de una manera activa a través de:
+    p.mb-5(data-aos="fade") El SENA tiene el espacio para que el egresado siga vinculado con la Entidad de una manera activa a través de:
+
+    .row.justify-content-center(data-aos="fade-down")
+      .col-1.col-number
+        .num 
+          span 1
+        .num-dots 
+      .col-md-7.col-11.pb-lg-5.pb-md-4
+        h3 Agencia pública de empleo – Observatorio laboral
+        p El Observatorio Laboral y Ocupacional del SENA, es el grupo de profesionales que vigila el comportamiento de las ocupaciones, partiendo de diversas fuentes de información del mercado laboral. 
+        p Provee información a nivel nacional, que contribuye a la orientación de acciones de formación y empleo.
+        p.mb-4 Mide el comportamiento de las ocupaciones a nivel nacional y departamental, con base en la información de inscritos (personas que buscan empleo) vacantes (requerimientos de recurso humano por parte de las empresas) y colocados (vinculaciones laborales exitosas) a través de la Agencia Pública de Empleo SENA.
+        .w-75.text-center.tema1-6.m-auto
+          a.anexo.mb-4.mb-lg-0(href="http://www.sena.edu.co/es-co/trabajo/Paginas/observatorioLaboral.aspx" target="_blank")
+            .anexo__icono
+              img(src="@/assets/curso/temas/icono-link.svg")
+            .anexo__texto
+              p.mb-0 #[b Enlace web.] Observatorio Laboral y Ocupacional del SENA
+      .col-md-4.col-9.pb-lg-5.pb-md-4.pb-5
+        img(src='@/assets/curso/temas/tema1-6_img.png', alt='')
+    
+    .row.justify-content-center(data-aos="fade-down")
+      .col-1.col-number
+        .num 
+          span 2
+        .num-dots  
+      .col-md-7.col-11.pb-lg-5.pb-md-4
+        h3 Oportunidades de emprendimiento
+        p El Fondo Emprender es un fondo de capital semilla creado por el Gobierno Nacional en el artículo 40 de la Ley 789 del 27 de diciembre de 2002: “por la cual se dictan normas para apoyar el empleo y ampliar la protección social y se modifican algunos artículos del Código Sustantivo de Trabajo”.
+        p.mb-4 Es así como el Fondo Emprender se constituyó como una cuenta independiente y especial adscrita al Servicio Nacional de Aprendizaje, SENA, el cual será administrado por esta entidad y cuyo objeto exclusivo será financiar iniciativas empresariales que provengan y sean desarrolladas por aprendices o asociaciones entre aprendices, practicantes universitarios o profesionales que su formación se esté desarrollando o se haya desarrollado en instituciones que para los efectos legales, sean reconocidas por el Estado de conformidad con las Leyes 30 de 1992 y 115 de 1994 y demás que las complementen, modifiquen o adicionen.
+      .col-md-4.col-9.pb-lg-5.pb-md-4.pb-5
+        img(src='@/assets/curso/temas/tema1-6_img2.png', alt='')
+    
+    .row.justify-content-center(data-aos="fade-down")
+      .col-1.col-number
+        .num 
+          span 3
+        .num-dots  
+      .col-md-7.col-11.pb-lg-5.pb-md-4
+        h3 Convenios para acceder a título nivel profesional
+        p.mb-4 Un egresado SENA puede acceder a los convenios con Instituciones de Educación Superior con el fin de realizar su ciclo profesional.  
+        .w-75.text-center.tema1-6.m-auto
+          a.anexo.mb-4.mb-lg-0(href="http://www.sena.edu.co/es-co/comunidades/egresados/Paginas/default.aspx" target="_blank")
+            .anexo__icono
+              img(src="@/assets/curso/temas/icono-link.svg")
+            .anexo__texto
+              p.mb-0 #[b Enlace web.]  Consultar convenios actuales.
+      .col-md-4.col-9.pb-lg-5.pb-md-4.pb-5
+        img(src='@/assets/curso/temas/tema1-6_img3.png', alt='')
+
+    .row.justify-content-center.mb-4(data-aos="fade-down")
+      .col-1.col-number
+        .num 
+          span 4
+      .col-md-7.col-11.pb-lg-5.pb-md-4
+        h3 Internacionalización
+        p El SENA gestiona la Internacionalización en los ámbitos tecnológicos, académicos y de transferencia de conocimientos, así como los proyectos de cooperación al desarrollo en el marco de la política exterior de Colombia. 
+        p.mb-4 Lo anterior con el objetivo de estar a la vanguardia en innovación y tecnología en el sector productivo, el SENA suscribe acuerdos con los mejores del mundo. 
+        .w-75.text-center.tema1-6.m-auto
+          a.anexo.mb-4.mb-lg-0(href="https://www.sena.edu.co/es-co/sena/Paginas/internacionalizacion.aspx" target="_blank")
+            .anexo__icono
+              img(src="@/assets/curso/temas/icono-link.svg")
+            .anexo__texto
+              p.mb-0 #[b Enlace web.] Información relacionada con la internacionalización 
+      .col-md-4.col-9.pb-lg-5.pb-md-4.pb-5
+        img(src='@/assets/curso/temas/tema1-6_img4.png', alt='')
+
+
+    p(data-aos="fade") Los egresados SENA son la prueba fehaciente que la formación profesional integral impartida en la Entidad es la de más alta reputación en el sector productivo.
+
+
 </template>
 
 <script>
